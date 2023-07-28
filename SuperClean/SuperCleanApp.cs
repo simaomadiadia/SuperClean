082d465d1804f16a722618745029cc925d5c78da
Elsondagama <Elsondagama@gmail.com>
2023-07-28 21:33:09 +0100

    SuperCleanApp criada

---
+++ SuperClean/SuperCleanApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperClean
+{
+    internal class SuperCleanApp
+    {
+
+        private Dictionary<string, Residencia> usersHomes;
+        
+        public SuperCleanApp() 
+        { 
+            usersHomes = new Dictionary<string, Residencia>();
+        }
+    }
+} 
